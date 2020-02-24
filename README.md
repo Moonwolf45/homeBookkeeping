99905a92e38bfa21940ea76411c54de29c0fe1bf
Vladislav <miznikov.1995@mail.ru>
2020-02-24 21:15:18 +0500

    Добавлено несколько плагинов для удобства разработки и работы проекта

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # HomeBookkeeping
 
-###Frontend
+## Frontend
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.1.
 
-###Backend
+## Backend
 This project was generated with NodeJS Express 4.17.1
 
--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const financesRoutes = require('./routes/finances');
 const financeCalendarRoutes = require('./routes/financeCalendar');
+const app = express();
 
-app.use('api/auth', authRoutes);
-app.use('api/analytics', analyticsRoutes);
-app.use('api/finances', financesRoutes);
-app.use('api/financeCalendar', financeCalendarRoutes);
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/finances', financesRoutes);
+app.use('/api/financeCalendar', financeCalendarRoutes);
 
 module.exports = app;
