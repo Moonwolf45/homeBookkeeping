6da616ff4c2edcb65ff311b26f2cf5fe06b49c33
Vladislav <miznikov.1995@mail.ru>
2020-03-15 15:07:52 +0500

    Созданы модели

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const firebase = require("firebase/app");
 
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const financesRoutes = require('./routes/finances');
 const financeCalendarRoutes = require('./routes/financeCalendar');
+const keys = require('./config/keys');
 const app = express();
+
+firebase.initializeApp(keys.firebaseConfig);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
---
+++ config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    firebaseConfig: {
+        apiKey: "AIzaSyD_yKAM5y2nY2bwk15sqfghKO0GboZ3B2w",
+        authDomain: "homebookkeeping-bfd1e.firebaseapp.com",
+        databaseURL: "https://homebookkeeping-bfd1e.firebaseio.com",
+        projectId: "homebookkeeping-bfd1e",
+        storageBucket: "homebookkeeping-bfd1e.appspot.com",
+        messagingSenderId: "690808670983",
+        appId: "1:690808670983:web:a5dc6948eda5ae188b53a2"
+    }
+};
